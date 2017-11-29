da916522aee083a22fab1f0239d78465cea5c24e
Leon <leon.sy.zhong@mail.foxconn.com>
2017-11-29 15:43:10 +0800

    trade 和 user_operation 的Model编写完成

--- apps/trade/models.py
+++ apps/trade/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from datetime import datetime
 
+from django.contrib.auth import get_user_model
+from goods.models import Goods
+User = get_user_model()
 # Create your models here.
+
+
+class ShoppingCart(models.Model):
+    """
+    购物车
+    """
+    user = models.ForeignKey(User, verbose_name="")
+    goods = models.ForeignKey(Goods)
+    goods_num = models.IntegerField(default=0, verbose_name="加购商品数量")
+
+    add_time = models.DateTimeField(default=datetime, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "购物车"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "%s(%d)".format(self.goods.name, self.goods_num)
+
+
+class OrderInfo(models.Model):
+    """
+    订单信息
+    """
+    ORDER_STATUS = (
+        ("success", "成功"),
+        ("cancel", "取消"),
+        ("to pay", "待支付")
+    )
+    # PAY_TYPE = (
+    #     ("wechat", "微信支付"),
+    #     ("alipay", "支付宝"),
+    # )
+    user = models.ForeignKey(User, verbose_name="用户")
+    order_sn = models.CharField(max_length=30, unique=True, verbose_name="订单编号")
+    trade_no = models.CharField(max_length=100, unique=True, null=True, blank=True, verbose_name="第三方支付产生的订单编号")
+    pay_status = models.CharField(choices=ORDER_STATUS, max_length=10, verbose_name="订单支付状态")
+    post_script = models.CharField(max_length=200, verbose_name="订单留言/备注")
+    order_mount = models.FloatField(default=0.0, verbose_name="订单金额")
+    pay_time = models.DateTimeField(null=True, blank=True,verbose_name="支付时间")
+
+    # 用户相关信息
+    address = models.CharField(max_length=100, default="", verbose_name="收货地址")
+    signer_name = models.CharField(max_length=20, default="", verbose_name="收件人")
+    signer_mobile = models.CharField(max_length=11, verbose_name="联系电话")
+
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "订单信息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return str(self.order.order_sn)
+
+
+class OrderGoods(models.Model):
+    """
+    订单中商品详情
+    """
+    order = models.ForeignKey(OrderInfo)
+    goods = models.ForeignKey(Goods)
+    goods_num = models.IntegerField(default=0, verbose_name="商品数量")
+
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "订单中商品详情"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return str(self.order.order_sn)
+
--- apps/user_operation/models.py
+++ apps/user_operation/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from goods.models import Goods
+from datetime import datetime
 
+User = get_user_model()
 # Create your models here.
+
+"""
+    用户操作，包括收藏，留言
+ """
+
+
+class UserFavor(models.Model):
+    """
+    收藏
+    """
+    user = models.ForeignKey(User, verbose_name="用户")
+    goods = models.ForeignKey(Goods, verbose_name="商品")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户收藏"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.user.name
+
+
+class UserLeavingMessage(models.Model):
+    """
+    留言信息
+    """
+    MSG_CHOICES = (
+        (1, "留言"),
+        (2, "询问"),
+        (3, "投诉"),
+        (4, "售后"),
+        (5, "求购")
+    )
+    user = models.ForeignKey(User, verbose_name="用户")
+    msg_type = models.CharField(default=1, choices=MSG_CHOICES, verbose_name="留言类型")
+    subject = models.CharField(max_length=30, default=" ", verbose_name="留言标题")
+    message = models.TextField(default=" ", verbose_name="留言内容", help_text="留言内容")
+    file = models.FileField(upload_to="")
+
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "留言信息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.subject
+
+
+class Address(models.Model):
+    """
+    收货地址
+    """
+    user = models.ForeignKey(User, verbose_name="用户")
+    district = models.CharField(default="", max_length=100, verbose_name="配送区域")
+    address = models.CharField(default="", max_length=100, verbose_name="详细地址")
+    signer_name = models.CharField(default="", max_length=30, verbose_name="收货人姓名")
+    signer_mobile = models.CharField(default="",max_length=11, verbose_name="收货人电话")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "收货地址"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.address
+
